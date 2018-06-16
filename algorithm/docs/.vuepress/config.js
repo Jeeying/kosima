b698671fc95b5360fb93edaba319e9a18ddb3ebe
lucy <qwer558a1@gmail.com>
2018-06-16 18:17:55 +0800

    modify

--- algorithm/docs/.vuepress/config.js
+++ algorithm/docs/.vuepress/config.js
@@ -6,7 +6,7 @@ module.exports = {
     ['link', { rel: 'icon', href: '/logo-kosima.png' }]
   ],
   serviceWorker: true,
-  base: '/kasima/',
+  base: '/<REPO>/',
   // theme: 'vue',
   themeConfig: {
     repo: 'Jeeying/kasima',
